443cc15f41418871e0940701af1210510ce181f1
Youssef Anab <yousseflida0@gmail.com>
2019-04-11 13:44:07 +0100

    animations improved

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 window.addEventListener('load', () => {
-  var profile_img = document.querySelector('#profile-img');
   var header_heading = document.querySelector('#header-heading');
   var header_body_text = document.querySelector('#header-body-text');
 
-  profile_img.style.opacity = 1;
   setTimeout(() => {
-    header_heading.style.opacity = 1;
-    header_body_text.style.opacity = 1;
+    header_heading.classList.add('fadeIn');
+    header_body_text.classList.add('fadeIn');
   }, 500);
 }, false);
